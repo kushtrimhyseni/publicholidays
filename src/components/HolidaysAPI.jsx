df794bc3fcf977a27c89d51e306e9f6ca8d6ad5b
kushtriimhyseni <kushtrim@starlabspro.com>
2022-04-19 01:35:54 +0200

    Added Spinner Component

--- src/components/HolidaysAPI.jsx
+++ src/components/HolidaysAPI.jsx
@@ -2,11 +2,12 @@ import React, { useState } from "react";
 import axios from "axios";
 import { countries } from "../countries";
 import SelectCountry from "./SelectCountry";
-
+import Spinner from "./Spinner";
 const HolidaysAPI = () => {
   const [holidays, setHolidays] = useState([]);
   const [error, setError] = useState("");
   const [isSelected, setSelected] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const getHolidays = async (selectedCountry) => {
     try {
@@ -15,8 +16,9 @@ const HolidaysAPI = () => {
       );
       setHolidays(response.data.response.holidays);
       setError("");
+      setLoading();
     } catch (err) {
-      setError("Country not found!");
+      setError("No data for this country!");
     }
   };
   const formatDate = (string) => {
@@ -27,62 +29,67 @@ const HolidaysAPI = () => {
   const handleSelect = (event) => {
     getHolidays(event.target.value);
     setSelected(true);
+    setLoading(true);
   };
-  return (
-    <div>
-      {error ? (
-        <div>{error}</div>
-      ) : (
-        <section className="container mx-auto mt-6 p-6 md:pt-0">
-          <SelectCountry
-            options={countries}
-            onChange={handleSelect}
-            disabledIndex={0}
-          />
-          {isSelected && (
-            <div className="w-full mb-8 overflow-hidden rounded-lg shadow-lg">
-              <div className="w-full overflow-x-auto">
-                <table className="w-full">
-                  <thead>
-                    <tr className="text-md font-semibold tracking-wide text-left text-gray-900 bg-[#7856ff] uppercase border-b border-gray-600">
-                      <th className="px-4 py-3">Holiday</th>
-                      <th className="px-4 py-3">Date</th>
-                      <th className="px-4 py-3">Details</th>
-                    </tr>
-                  </thead>
-                  <tbody className="bg-white">
-                    {holidays?.map((holiday) => {
-                      return (
-                        <tr className="text-gray-700">
-                          <td className="px-4 py-3 border">
-                            <div className="flex items-center text-sm">
-                              <div>
-                                <p className="font-semibold text-black">
-                                  {holiday.name}
-                                </p>
+  if (!loading) {
+    return (
+      <div>
+        {error ? (
+          <div>{error}</div>
+        ) : (
+          <section className="container mx-auto mt-6 p-6 md:pt-0">
+            <SelectCountry
+              options={countries}
+              onChange={handleSelect}
+              disabledIndex={0}
+            />
+            {isSelected && (
+              <div className="w-full mb-8 overflow-hidden rounded-lg shadow-lg">
+                <div className="w-full overflow-x-auto">
+                  <table className="w-full">
+                    <thead>
+                      <tr className="text-md font-semibold tracking-wide text-left text-gray-900 bg-[#7856ff] uppercase border-b border-gray-600">
+                        <th className="px-4 py-3">Holiday</th>
+                        <th className="px-4 py-3">Date</th>
+                        <th className="px-4 py-3">Details</th>
+                      </tr>
+                    </thead>
+                    <tbody className="bg-white">
+                      {holidays?.map((holiday) => {
+                        return (
+                          <tr className="text-gray-700">
+                            <td className="px-4 py-3 border">
+                              <div className="flex items-center text-sm">
+                                <div>
+                                  <p className="font-semibold text-black">
+                                    {holiday.name}
+                                  </p>
+                                </div>
                               </div>
-                            </div>
-                          </td>
-                          <td className="px-4 py-3 text-ms font-semibold border">
-                            {formatDate(holiday.date.iso)}
-                          </td>
-                          <td className="px-4 py-3 text-xs border">
-                            <span className="px-2 py-1 font-semibold  text-black text-sm rounded-sm">
-                              {holiday.description}
-                            </span>
-                          </td>
-                        </tr>
-                      );
-                    })}
-                  </tbody>
-                </table>
+                            </td>
+                            <td className="px-4 py-3 text-ms font-semibold border">
+                              {formatDate(holiday.date.iso)}
+                            </td>
+                            <td className="px-4 py-3 text-xs border">
+                              <span className="px-2 py-1 font-semibold  text-black text-sm rounded-sm">
+                                {holiday.description}
+                              </span>
+                            </td>
+                          </tr>
+                        );
+                      })}
+                    </tbody>
+                  </table>
+                </div>
               </div>
-            </div>
-          )}
-        </section>
-      )}
-    </div>
-  );
+            )}
+          </section>
+        )}
+      </div>
+    );
+  } else {
+    return <Spinner />;
+  }
 };
 
 export default HolidaysAPI;
