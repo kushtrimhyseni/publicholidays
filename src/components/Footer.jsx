927634093a118665ca0cd8260747f68f53788feb
kushtriimhyseni <kushtrim@starlabspro.com>
2023-04-19 04:10:12 +0200

    Added current year dynamically

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
         </div>
         <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © 2022{" "}
+          © {new Date().getFullYear()}
           <a href="https://kushtrimhyseni.com" class="hover:underline">
             frosty™
           </a>
